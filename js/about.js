74a62cfce405cadc069c9805b163792a5e72c510
rutuja091 <gaikwadrutuja254@gmail.com>
2024-04-11 00:06:00 +0530

    hide part complete

---
+++ js/about.js
@@ -0,0 +1,12 @@
+function datta1(X){
+    if(X==0)
+    document.getElementById('datta').style.display='block';
+  
+  
+else
+document.getElementById('datta').style.display='none';
+return;
+}
+
+
+
