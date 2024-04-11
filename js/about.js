85f60fecdb20ed258c7034ddca106690958338eb
rutuja091 <gaikwadrutuja254@gmail.com>
2024-04-11 12:28:45 +0530

    add 4th function

--- js/about.js
+++ js/about.js
@@ -15,6 +15,9 @@ else
 document.getElementById("bushra0").style.display='none';
 return;
 }
+
+
+
 function sahil1(X){
     if (X==0)
     document.getElementById("sahil0").style.display='block'; 
@@ -23,7 +26,7 @@ function sahil1(X){
     return;
 }
 
-
+function sahil1(X)
 
 
 
