f7e53dc323626e4ed782f7cbda0ac9639e1b3e87
rutuja091 <gaikwadrutuja254@gmail.com>
2024-04-11 11:22:22 +0530

    add id

--- js/about.js
+++ js/about.js
@@ -1,9 +1,17 @@
 function datta1(X)
 {
     if (X==0)
-    document.getElementById("datta").style.display='block';
+    document.getElementById("datta0").style.display='block';
 else
-    document.getElementById("datta").style.display='none';
+    document.getElementById("datta0").style.display='none';
 return;
 }
+
+
+
+
+
+
+
+
 
