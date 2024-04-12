7a4978a240eeb334ca0b9f7b6c0a5ef54e9c4939
dattanedre <dattatraynedre@gmail.com>
2024-04-12 17:16:15 +0530

    js modified

--- js/accessory.js
+++ js/accessory.js
@@ -1,36 +1,4 @@
 
-function inc() {
-    const quantityText = document.getElementById("quantity-text");
-    const showQuantity = document.getElementById("show-quantity");
-    const showTotal = document.getElementById("show-total");
-
-    let quantity = parseInt(quantityText.innerHTML);
-    if (quantity < 5) {
-        quantityText.innerHTML = ++quantity;
-    }
-    else {
-        alert("Bss ho gya !!");
-    }
-    showQuantity.innerText = `${quantity} x 3.43L`;
-    showTotal.innerText=`Total: ${quantity * 3.43} L `
-}
-
-function dec() {
-    const quantityText = document.getElementById("quantity-text");
-    const showQuantity = document.getElementById("show-quantity");
-    const showTotal = document.getElementById("show-total");
-
-    let quantity = parseInt(quantityText.innerHTML);
-    if (quantity > 1) {
-        quantityText.innerHTML = --quantity;
-    }
-    else {
-        alert("You can't add less than 1 item")
-    }
-    showQuantity.innerText = `${quantity} x 3.43`;
-    showTotal.innerText=`Total: ${quantity * 3.43} L `
-
-}
 
 const quantityElement = document.getElementById("quantity");
 function increment() {
